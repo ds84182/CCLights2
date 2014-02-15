47b271561fde09d4e52df08a39dc80c1bbb1065d
alekso56 <simple_and_easy@hotmail.com>
2014-02-15 19:23:50 +0100

    oops?

--- src/ds/mods/CCLights2/jhlabs/image/ConvolveFilter.java
+++ src/ds/mods/CCLights2/jhlabs/image/ConvolveFilter.java
@@ -77,7 +77,6 @@ public class ConvolveFilter extends AbstractBufferedImageOp {
 		return edgeAction;
 	}
 
-    @Override
 	public BufferedImage filter( BufferedImage src, BufferedImage dst ) {
         int width = src.getWidth();
         int height = src.getHeight();
@@ -95,19 +94,16 @@ public class ConvolveFilter extends AbstractBufferedImageOp {
         return dst;
     }
 
-    @Override
 	public BufferedImage createCompatibleDestImage(BufferedImage src, ColorModel dstCM) {
         if ( dstCM == null )
             dstCM = src.getColorModel();
         return new BufferedImage(dstCM, dstCM.createCompatibleWritableRaster(src.getWidth(), src.getHeight()), dstCM.isAlphaPremultiplied(), null);
     }
     
-    @Override
 	public Rectangle2D getBounds2D( BufferedImage src ) {
         return new Rectangle(0, 0, src.getWidth(), src.getHeight());
     }
     
-    @Override
 	public Point2D getPoint2D( Point2D srcPt, Point2D dstPt ) {
         if ( dstPt == null )
             dstPt = new Point2D.Double();
@@ -115,7 +111,6 @@ public class ConvolveFilter extends AbstractBufferedImageOp {
         return dstPt;
     }
 
-    @Override
 	public RenderingHints getRenderingHints() {
         return null;
     }
@@ -288,7 +283,6 @@ public class ConvolveFilter extends AbstractBufferedImageOp {
 		}
 	}
 
-	@Override
 	public String toString() {
 		return "Blur/Convolve...";
 	}
