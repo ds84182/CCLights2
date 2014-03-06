fcaa5c3692f97fe3a8dd176119223b6aaa0cd102
alekso56 <simple_and_easy@hotmail.com>
2014-03-06 22:07:53 +0100

    Fix Poke.

--- src/ds/mods/CCLights2/block/tileentity/TileEntityGPU.java
+++ src/ds/mods/CCLights2/block/tileentity/TileEntityGPU.java
@@ -443,33 +443,23 @@ public class TileEntityGPU extends TileEntity implements IPeripheral {
 			case 18: {
 				//import
 				double a = System.currentTimeMillis();
-				if (args.length > 1) {
+				if (args.length == 1) {
 					Byte[] data;
-					String format;
 					if (args[0] instanceof Map)
 					{
 						int size = 0;
 						//One of the things I hate is that ComputerCraft uses Doubles for all their values
 						Map m = (Map)args[0];
-						format = (String)args[1];
-						for (double i = 1; i<Double.MAX_VALUE; i++)
-						{
-							if (m.containsKey(i))
-								size = (int) i;
-							else
-								break;
-						}
-						data = new Byte[size];
+						data = new Byte[m.size()];
 						for (double i = 0; i<data.length; i++)
 						{
 							data[(int) i] = ((Double)m.get(i+1D)).byteValue();
 						}
-						CCLights2.debug("Moved data");
+						CCLights2.debug("Converted data");
 					}
 					else if (args[0] instanceof String)
 					{
 						String file = (String)args[0];
-						format = (String)args[1];
 						File f = new File(CCLights2.proxy.getWorldDir(worldObj),"computer/"+computer.getID()+"/"+file);
 						FileInputStream in = new FileInputStream(f);
 						byte[] b = new byte[(int)in.getChannel().size()];
@@ -479,12 +469,10 @@ public class TileEntityGPU extends TileEntity implements IPeripheral {
 					}
 					else
 					{
-						throw new Exception("import: Argument Error: (filedata or filename), format expected");
+						throw new Exception("import: Argument Error: (filedata or filename)");
 					}
 					DrawCMD cmd = new DrawCMD();
-					Object[] nargs = new Object[2];
-					nargs[0] = data;
-					nargs[1] = format;
+					Object[] nargs = new Object[]{data};
 					cmd.cmd = 14;
 					cmd.args = nargs;
 					int id = (Integer) gpu.processCommand(cmd)[0];
@@ -492,7 +480,7 @@ public class TileEntityGPU extends TileEntity implements IPeripheral {
 					Object[] ret = {id,tex.getWidth(),tex.getHeight()};
 					gpu.drawlist.push(cmd);
 					double b = System.currentTimeMillis();
-					System.out.println("Import time: "+(b-a)+"ms");
+					CCLights2.debug("Import time: "+(b-a)+"ms");
 					return ret;
 				}
 			}
